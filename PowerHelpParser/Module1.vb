809519effadf5bcd435e8e560d03d6ba3ba218b0
Gjuro Kladaric <gjuwindev@gmail.com>
2021-03-15 13:44:19 +0100

    Implemented reusing results, until result file is deleted. Moved PowerShell functionality into functions, so the main program stays clean for Get-Help command output processing.

--- PowerHelpParser/Module1.vb
+++ PowerHelpParser/Module1.vb
@@ -6,18 +6,55 @@ Module Module1
 
     Sub Main()
 
-        Dim pws = PowerShell.Create()
-        pws.AddScript("Get-Help Get-Mailbox -Full | Out-String")
+        Dim command As String = "Get-Help Get-Mailbox -Full"
 
-        Dim psobjects As ObjectModel.Collection(Of PSObject) = pws.Invoke
 
-        For Each pso In psobjects
-            Console.WriteLine(pso)
-        Next
+        Dim result As String = GetPowerShellResult(command)
+
+        ' will process the command output here
+        Console.WriteLine(result)
+
 
         Console.WriteLine("GOTOVO")
         Console.ReadLine()
 
     End Sub
+
+    Function GetPowerShellResult(command As String, Optional savedContentOverride As Boolean = False) As String
+
+        Dim powerShellResultsFilename As String = "powerShellResults"
+
+        Dim result As String
+
+        If Not savedContentOverride AndAlso
+            IO.File.Exists(powerShellResultsFilename) Then
+            result = IO.File.ReadAllText(powerShellResultsFilename)
+        Else
+            Dim pws As PowerShell = PowerShell.Create()
+
+            result = ExecutePowerShellCommand(pws, command)
+
+            IO.File.WriteAllText(powerShellResultsFilename, result)
+        End If
+
+        Return result
+
+    End Function
+
+    Function ExecutePowerShellCommand(pws As PowerShell, command As String) As String
+
+        Dim sb As New Text.StringBuilder
+
+        pws.AddScript(command & " | Out-String")
+
+        Dim psobjects As ObjectModel.Collection(Of PSObject) = pws.Invoke
+
+        For Each pso In psobjects
+            sb.AppendLine(pso.ToString)
+        Next
+
+        Return sb.ToString
+
+    End Function
 
 End Module
